5d43a4415c4cf64f70f6b377878188046051d2d7
Andrej <andrej_velevski@hotmail.com>
2020-06-13 15:07:21 +0200

    Chapter 1: Output an image

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	std::ofstream output;
+	output.open("output.ppm");
+
+	int nx = 200;
+	int ny = 100;
+	output << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; ++i)
+		{
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = 0.2f;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			output << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+	output.close();
 
 	return 0;
 }
