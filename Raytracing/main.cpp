9eaf8278b52fefa6e71e97b433cf55a5fdf28f63
Andrej <andrej_velevski@hotmail.com>
2020-06-13 16:14:35 +0200

    Chapter 2: The vec3 class

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -5,6 +5,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#include "vec3.h"
+
 int main()
 {
 	const unsigned int width = 200;
@@ -16,12 +18,11 @@ int main()
 	{
 		for (int x = 0; x < width; ++x)
 		{
-			float r = float(x) / float(width);
-			float g = float(y) / float(height);
-			float b = 0.2f;
-			int ir = int(255.99f * r);
-			int ig = int(255.99f * g);
-			int ib = int(255.99f * b);
+			vec3 color(float(x) / float(width), float(y) / float(height), 0.2f);
+
+			int ir = int(255.99f * color[0]);
+			int ig = int(255.99f * color[1]);
+			int ib = int(255.99f * color[2]);
 
 			data[3 * (y * width + x) + 0] = ir;
 			data[3 * (y * width + x) + 1] = ig;
