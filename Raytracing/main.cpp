79d363e6c36790513db276532132eb08f21cc8af
Andrej <andrej_velevski@hotmail.com>
2020-06-13 16:06:49 +0200

    Changed to stb_image

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
-#include <fstream>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 int main()
 {
-	std::ofstream output;
-	output.open("output.ppm");
+	const unsigned int width = 200;
+	const unsigned int height = 100;
+	const unsigned int channels = 3;
 
-	int nx = 200;
-	int ny = 100;
-	output << "P3\n" << nx << " " << ny << "\n255\n";
-	for (int j = ny - 1; j >= 0; j--)
+	unsigned char data[width * height * 3];
+	for (int y = 0; y < height; ++y)
 	{
-		for (int i = 0; i < nx; ++i)
+		for (int x = 0; x < width; ++x)
 		{
-			float r = float(i) / float(nx);
-			float g = float(j) / float(ny);
+			float r = float(x) / float(width);
+			float g = float(y) / float(height);
 			float b = 0.2f;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
-			output << ir << " " << ig << " " << ib << "\n";
+			int ir = int(255.99f * r);
+			int ig = int(255.99f * g);
+			int ib = int(255.99f * b);
+
+			data[3 * (y * width + x) + 0] = ir;
+			data[3 * (y * width + x) + 1] = ig;
+			data[3 * (y * width + x) + 2] = ib;
 		}
 	}
 
-	output.close();
+	stbi_write_png("output.png", width, height, channels, data, width * channels);
 
 	return 0;
 }
