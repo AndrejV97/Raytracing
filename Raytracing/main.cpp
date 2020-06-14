710d65f7a3c1c8b857a67312d37d75daf709f88b
Andrej <andrej_velevski@hotmail.com>
2020-06-14 12:54:41 +0200

    Chapter 5: Surface normals and multiple objects

--- Raytracing/main.cpp
+++ Raytracing/main.cpp
@@ -12,22 +12,33 @@ const unsigned int width = 200;
 const unsigned int height = 100;
 const unsigned int channels = 3;
 
-bool hit_sphere(const vec3& center, float radius, const ray& r)
+float hit_sphere(const vec3& center, float radius, const ray& r)
 {
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0f * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	return discriminant > 0;
+	float discriminant = b * b - 4.0f * a * c;
+	if (discriminant < 0.0f)
+	{
+		return -1.0f;
+	}
+	else
+	{
+		return (-b - sqrt(discriminant)) / (2.0f * a);
+	}
 }
 
 vec3 color(const ray& r)
 {
-	if (hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r))
-		return vec3(1.0f, 0.0f, 0.0f);
+	float t = hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r);
+	if (t > 0.0f)
+	{
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0.0f, 0.0f, -1.0f));
+		return 0.5f * vec3(N.x() + 1.0f, N.y() + 1.0f, N.z() + 1.0f);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
-	float t = 0.5f * (unit_direction.y() + 1.0f);
+	t = 0.5f * (unit_direction.y() + 1.0f);
 	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
@@ -39,12 +50,13 @@ int main()
 	vec3 origin(0.0f, 0.0f, 0.0f);
 
 	unsigned char data[width * height * 3];
+	unsigned idx = 0;
 	for (int y = 0; y < height; ++y)
 	{
 		for (int x = 0; x < width; ++x)
 		{
-			float u = float(x) / width;
-			float v = float(y) / height;
+			float u = float(x) / float(width);
+			float v = float(y) / float(height);
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
 			vec3 col = color(r);
 			int ir = int(255.99f * col[0]);
